3b8ce064047c0991acf18d0409017b91aaa0aea9
Josh Justice <josh@need-bee.com>
2014-05-29 17:04:36 -0400

    adds cdn_url method for JS

--- src/NpmWeb/CdnHelper/helpers.php
+++ src/NpmWeb/CdnHelper/helpers.php
@@ -11,11 +11,30 @@ if ( ! function_exists('cdn_asset'))
 	 */
 	function cdn_asset($filename,$folder = null)
 	{
-		$url = Config::get('cdn.base_url');
+		$url = '';
 		if($folder) {
 			$url .= $folder;
 		}
 		$url .= esc_url($filename);
+		return cdn_url($url);
+	}
+}
+
+if ( ! function_exists('cdn_url'))
+{
+	/**
+	 * Generate a URL on a CDN.
+	 *
+	 * @param  string  $name
+	 * @param  string  $folder
+	 * @return string
+	 */
+	function cdn_url($path = null)
+	{
+		$url = Config::get('cdn.base_url');
+		if( null !== $path ) {
+			$url .= $path;
+		}
 		return $url;
 	}
 }
